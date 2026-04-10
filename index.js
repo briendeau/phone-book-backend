21ea6cd0b01d624095a1966b420057290eb8be21
briendeau <yamori425@gmail.com>
2026-04-10 17:28:36 -0400

    initial backend setup

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+
+app.use(morgan(':body'))
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
+
+app.use(cors())
+
+let entries = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(entries)
+})  
+
+app.get('/info', (request, response) => {
+  response.send(`
+  <h2>Phonebook has info for ${entries.length} people</h2>
+  <p>${new Date()}
+  </p>
+ `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = entries.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  entries = entries.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'name or number is missing' })
+  }
+  const duplicate = entries.some((person) => person.name === body.name)
+  if (duplicate) {
+    return response.status(400).json({ error: 'name must be unique' })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.random().toString(36).substring(2, 15),
+  }
+  entries = entries.concat(person)
+  response.json(person)
+})
+
+
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
