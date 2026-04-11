ef49870f8c16edbaabe1c9096f57e36218a1474b
briendeau <yamori425@gmail.com>
2026-04-11 17:02:22 -0400

    removed / route so static html is served.

--- index.js
+++ index.js
@@ -31,10 +31,6 @@ let entries = [
     }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/persons', (request, response) => {
   response.json(entries)
 })  
